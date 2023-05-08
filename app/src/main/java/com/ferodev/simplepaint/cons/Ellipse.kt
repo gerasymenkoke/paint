d2065d758bfbf63e24bde70c4424b7965c61482d
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-08 21:49:45 +0300

    Add files via upload

---
+++ app/src/main/java/com/ferodev/simplepaint/cons/Ellipse.kt
@@ -0,0 +1,9 @@
+package com.ferodev.simplepaint.cons
+
+data class Ellipse(
+    var startX: Float = 0f,
+    var startY: Float = 0f,
+    var stopX: Float = 0f,
+    var stopY: Float = 0f,
+    var color: Int = 0
+)
