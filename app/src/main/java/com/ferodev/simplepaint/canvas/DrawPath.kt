bbda92d86fba6f06f65dd1f23387711914fafeb8
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-09 21:32:10 +0300

    Update DrawPath.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPath.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPath.kt
@@ -20,8 +20,8 @@ class DrawPath @JvmOverloads constructor(
         var pathList = ArrayList<Path>()
     }
 
-    private var mX = 0f
-    private var mY = 0f
+    private var mX = 20f
+    private var mY = 20f
 
     private val paintBrush = Paint().apply {
         isAntiAlias = true
