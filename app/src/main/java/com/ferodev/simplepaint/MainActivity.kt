b02e590f338cbdf7920a97a901f1235c639a9a10
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-12 20:55:57 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -3,10 +3,10 @@ package com.ferodev.simplepaint
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+//import android.support.v4.app.ActivityCompat
+//import android.support.v4.content.ContextCompat
 
 
 import android.graphics.Color
