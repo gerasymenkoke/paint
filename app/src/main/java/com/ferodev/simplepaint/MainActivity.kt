1ae4522f12d4f8b8b62a374d13446a2989f1e280
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-14 09:16:10 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -1,14 +1,5 @@
 package com.ferodev.simplepaint
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-//import android.support.v7.app.AppCompatActivity
-//import android.os.Bundle
-//import android.support.v4.app.ActivityCompat
-//import android.support.v4.content.ContextCompat
-
-
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
@@ -19,59 +10,6 @@ import com.ferodev.simplepaint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    
-    
-        companion object {
-        const val REQUEST_PERMISSION = 1
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSION)
-        } else {
-            write()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            REQUEST_PERMISSION -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                write()
-            }
-        }
-    }
-
-    private fun write() {
-        val dir = "${Environment.getExternalStorageDirectory()}/$packageName"
-        File(dir).mkdirs()
-        val file = "%1\$tY%1\$tm%1\$td%1\$tH%1\$tM%1\$tS.log".format(Date())
-        File("$dir/$file").printWriter().use { 
-            it.println("text")
-        }
-    }
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -274,4 +212,7 @@ class MainActivity : AppCompatActivity() {
         path = Path()
     }
 
-}
+}  
+            
+
+    
