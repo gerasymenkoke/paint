5b7afe1d5757430a16975121a8b1534937fd1eb0
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-22 21:11:07 +0300

    Update DrawPencil.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
@@ -68,8 +68,15 @@ class DrawPencil @JvmOverloads constructor(
           override fun onTouchEvent(event: MotionEvent): Boolean {
         val x = event.x
         val y = event.y
-        var x = DrawPencil.x  
-              x = x.toString()
+        
+              object SharedData {
+    var myString = x.toString()
+                                }              
+        
+       
+        
+        
+        
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 touchStart(x, y)
