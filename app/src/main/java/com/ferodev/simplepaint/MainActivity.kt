19f4a97cefc84bd312cd2797dacae26835fbb41a
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-26 21:09:58 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -63,7 +63,7 @@ class MainActivity : AppCompatActivity() {
            
                count=count + 1 
                
-               val x = DrawPencil.onTouchEvent()
+               val x = DrawPencil().onTouchEvent
            //    val xxReturn = xxx.onTouchEvent()
            //    val xMember = xxx.x 
                 
