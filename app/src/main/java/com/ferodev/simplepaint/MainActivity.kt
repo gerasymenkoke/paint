56d024c69672ff136b1f09fa8af998762143f972
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-27 16:04:55 +0400

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -101,7 +101,7 @@ class MainActivity : AppCompatActivity() {
                    
                     count=count + 1 
                     drawPencil1.visibility = count  
-                    drawPencil1.text = "count"  
+                    drawPencil1.test = "count"  
             
                    
                    
