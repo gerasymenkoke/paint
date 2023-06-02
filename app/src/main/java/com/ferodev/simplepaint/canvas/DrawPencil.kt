a1d322b1890d5c17df6e83a5cb67bde97de5e537
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-02 19:29:02 +0300

    Update DrawPencil.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
@@ -53,7 +53,7 @@ class DrawPencil @JvmOverloads constructor(
     private var xx = "99"
     
      companion object {
-        var xxx = xx
+        var xxx = com.ferodev.simplepaint.canvas.DrawPencil.xx
      }
     
     
