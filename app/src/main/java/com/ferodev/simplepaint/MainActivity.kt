fc2426b8f7330df95557e9d29687c38040a6a5ab
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-03 08:55:44 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             
-          drawPencil.setOnClickListener {
+          drawPencil.setOnCheckedChangeListener {
           textviewid.text = xxx + "  " + yyy
                                         }
                                       
