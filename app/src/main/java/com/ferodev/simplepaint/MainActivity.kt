3145706517c6e65134391f6db06c6f0e4729816d
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-05 14:55:14 +0400

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -68,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                             
                  //    btn = zzz                  
             
-          protected void  onVisibilityChanged (btnPencil, btn)
+          private fun   onVisibilityChanged(btnPencil: View, btn: int)
                  {  textviewid.text = xxx + "  " + yyy + "  " + zzz }
                  
                  
