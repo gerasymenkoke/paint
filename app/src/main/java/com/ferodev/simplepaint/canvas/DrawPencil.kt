039fffea713608e804f1ac544c1e63232e63fb2c
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-05 17:23:06 +0400

    Update DrawPencil.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
@@ -21,7 +21,7 @@ import android.graphics.Path
 
 import com.ferodev.simplepaint.MainActivity.Companion.currentBrush
 import com.ferodev.simplepaint.MainActivity.Companion.path
-import com.ferodev.simplepaint.MainActivity.Companion.btn
+// import com.ferodev.simplepaint.MainActivity.Companion.btn
 
 
 import com.ferodev.simplepaint.cons.Pencil
