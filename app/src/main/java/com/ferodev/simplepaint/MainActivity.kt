573da871d26e825734460c5077147f96ec4bbe39
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-09 21:26:07 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -198,14 +198,14 @@ class MainActivity : AppCompatActivity() {
                      drawPencil.setOnTouchListener { v, event ->
    //       val action = event.action
            when(action){
-          //  MotionEvent.ACTION_MOVE -> {  
+           MotionEvent.ACTION_MOVE -> {  
                     
             //         val x = event.x
        // val y = event.y
        // xxx = x.toString()       
        // yyy = y.toString()    
                         
-             ->       textviewid.text = xxx + "  " + yyy + " drawPencil "  }
+                   textviewid.text = xxx + "  " + yyy + " drawPencil "  }
               //MotionEvent.ACTION_CANCEL -> {
 
                 //}
