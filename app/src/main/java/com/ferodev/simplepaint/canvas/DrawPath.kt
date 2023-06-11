6a8e23d0d027861dc5d8e7f37c2bbc53cf3320a8
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-11 21:43:26 +0300

    Update DrawPath.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPath.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPath.kt
@@ -10,7 +10,7 @@ import android.view.View
 import com.ferodev.simplepaint.MainActivity.Companion.colorList
 import com.ferodev.simplepaint.MainActivity.Companion.currentBrush
 import com.ferodev.simplepaint.MainActivity.Companion.path
-import com.ferodev.simplepaint.MainActivity.Companion.textviewid
+import com.ferodev.simplepaint.MainActivity.Companion.textviewid.text
 
 class DrawPath @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
