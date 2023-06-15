ab34cfc84a894a61fc91c9a8d8b327b8ee88ef09
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-15 21:29:05 +0300

    Update DrawRectangle.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawRectangle.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawRectangle.kt
@@ -18,7 +18,7 @@ class DrawRectangle @JvmOverloads constructor(
 
    
          companion object {
-        var xxx = "vvv"    
+        var xxx = r.startX.toString()  
        // var btn =  1  
         
                       }
@@ -79,7 +79,7 @@ class DrawRectangle @JvmOverloads constructor(
             
             
    
-     xxx = r.startX.toString()
+     //xxx = r.startX.toString()
     MainActivity()
     
     
