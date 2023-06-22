c54dd13c16f363c9a619ef60265630812ed247f3
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-22 21:09:15 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -243,7 +243,7 @@ class MainActivity : AppCompatActivity() {
 //var j = 1
  //   while (j<=3)  {
    //     j=j+1 
-textviewid.text = aaa[1] + " " aaa[2] + " " + aaa[3] + " " + aaa[4] + " " + aaa[5] + " " + aaa[6] + " " + aaa[7] + " "  + aaa[8] + " "  + aaa[9] + " " + aaa[10] 
+textviewid.text = aaa[1] + " " + aaa[2] + " " + aaa[3] + " " + aaa[4] + " " + aaa[5] + " " + aaa[6] + " " + aaa[7] + " "  + aaa[8] + " "  + aaa[9] + " " + aaa[10] 
       //   }
 
 
