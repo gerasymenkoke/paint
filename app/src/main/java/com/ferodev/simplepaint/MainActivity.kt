450596012f7d5988d976e9be5774673a402de218
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-06-25 09:21:07 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -150,10 +150,6 @@ textviewid.text = aaa[0] + " " +aaa[1] + " " + aaa[2] + " " + aaa[3] + " " + aaa
 
 
 
-private fun currentColor(color: Int) {
-        currentBrush = color
-        path = Path()
-    }
 
 
 
