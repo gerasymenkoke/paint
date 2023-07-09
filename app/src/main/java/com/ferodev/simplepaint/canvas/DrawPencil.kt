e3852a61559b82a026578415b251550dbb497b17
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-07-09 11:12:12 +0300

    Update DrawPencil.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
@@ -149,10 +149,9 @@ class DrawPencil @JvmOverloads constructor(
          rxx = rx.toString()       
          ryy = ry.toString()
          
-//       if ( ((rx<=0 && rx1<=0) && (ry<=0 && ry1<=0)) ||  ((rx>=0 && rx1>=0) && (ry>=0 && ry1>=0)) || ((rx>=0 && rx1>=0) && (ry<=0 && ry1<=0)) ||
-  //              ((rx<=0 && rx1<=0) && (ry>=0 && ry1>=0)) )  { j=j }
+      if ( (rx==rx1) && (ry==ry1))  { j=j }
          
-    //      else    {  
+          else    {  
                       crx_[j] = rx
                       cry_[j] = ry
                       x1=x
@@ -161,7 +160,7 @@ class DrawPencil @JvmOverloads constructor(
                       ry1=ry
                       j = j + 1 
                 
-      //            }
+                  }
                            
          
              }
