502f2342da771d9460f5a368032adf1aede1c4a9
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-07-13 21:13:58 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -204,7 +204,9 @@ while (jj >=0 && jj<=1)
     while (j >=0 && j<=9) {
     
     if ( (crx_[j] > 0 && rrx[jj][j] > 0) ||  (crx_[j] < 0 && rrx[jj][j] < 0) || ( cry_[j]  > 0 && rry[jj][j] > 0) ||  (cry_[j] < 0  && rry[jj][j] < 0) )   { j=j } 
-                                    else  { res[jj] = res[jj] +  Math.abs (crx_[j] - rrx[jj][j])  }
+                                    else  { res[jj] = res[jj] +  Math.abs (crx_[j] - rrx[jj][j]) 
+                                            res[jj] = res[jj] +  Math.abs (cry_[j] - rry[jj][j]) 
+                                          }
                                         // Math.abs (crx_[j] - rrx[jj][j])  }
   
      j=j+1
