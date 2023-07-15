e31b0335529f7de306a4fcee5d45718a292a08de
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-07-15 09:03:11 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -146,7 +146,7 @@ class MainActivity : AppCompatActivity() {
                            rraaa[jjj]= "(" +  rrx[jj][jjj].toString() + ")"  
                            jjj=jjj+1
                           }
-             calc.text = "0: " + rraaa[0] + " " + rraaa[1] + " " + rraaa[2] + " " + rraaa[3] + " " + rraaa[4] + " " + rraaa[5] + " " + rraaa[6] + " " + rraaa[7] + " "  + rraaa[8] + " "  + rraaa[9]  
+             // calc.text = "0: " + rraaa[0] + " " + rraaa[1] + " " + rraaa[2] + " " + rraaa[3] + " " + rraaa[4] + " " + rraaa[5] + " " + rraaa[6] + " " + rraaa[7] + " "  + rraaa[8] + " "  + rraaa[9]  
  // Filling sample array raa
 // 1
       jj = 1                  
@@ -161,7 +161,7 @@ class MainActivity : AppCompatActivity() {
                            rraaa[jjj]= "(" +   rrx[jj][jjj].toString()  + ")"  
                            jjj=jjj+1
                           }
-              calc.text = "1: " + rraaa[0] + " " + rraaa[1] + " " + rraaa[2] + " " + rraaa[3] + " " + rraaa[4] + " " + rraaa[5] + " " + rraaa[6] + " " + rraaa[7] + " "  + rraaa[8] + " "  + rraaa[9]             
+              calc.text =  "0: " + rraaa[0] + " " + rraaa[1] + " " + rraaa[2] + " " + rraaa[3] + " " + rraaa[4] + " " + rraaa[5] + " " + rraaa[6] + " " + rraaa[7] + " "  + rraaa[8] + " "  + rraaa[9] + "                        " + "1: " + rraaa[0] + " " + rraaa[1] + " " + rraaa[2] + " " + rraaa[3] + " " + rraaa[4] + " " + rraaa[5] + " " + rraaa[6] + " " + rraaa[7] + " "  + rraaa[8] + " "  + rraaa[9]             
 
 
 
