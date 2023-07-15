f23b12369e6353e74682e0894380101ff57fbbfa
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-07-15 21:38:59 +0300

    Update DrawPencil.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
@@ -152,7 +152,7 @@ class DrawPencil @JvmOverloads constructor(
       if ( ((rx==rx1) && (ry==ry1)) || ((x==x1) && (y==y1)) ) { j=j }
 
          
-          else    {   if (N==3)
+          else    {   if (N==1)
                      { crx_[j] = rx
                       cry_[j] = ry
                       crx_[j] = crx_[j] + cry_[j]  
