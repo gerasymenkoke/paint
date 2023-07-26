c85f24ebbeacd0467f2e85e8720403ff2cd26fe0
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-07-26 21:40:39 +0300

    Update DrawPencil.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
@@ -136,7 +136,7 @@ class DrawPencil @JvmOverloads constructor(
         xxx = x.toString()       
         yyy = y.toString()    
 
-      if ( (x-x1)/x < 0.05f && (y-y1)/x < 0.05f)
+      if ( (x-x1)/x > 0.05f || (y-y1)/y > 0.05f)
         {  
         
             if (j >=1 && j<=19)
