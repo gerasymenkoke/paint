bffbdb9ca056166be68003d769eee84208f35ca1
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-08-11 21:24:05 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -100,9 +100,9 @@ class MainActivity : AppCompatActivity() {
      jj = 0                  
      
      dir_rr[0][0] = arrayOf<Int>(  3,  0,  1,  2,  10,  10, 10, 10, 10, 10)
-     rr[0][0] = arrayOf<Float>(  -0.03f,  0.03f,  0.03f,  -0.03f, 0.0f, 0.01f, 0.0f, 0.0f, 0.0f, 0.0f)
+     rr[0][0] = arrayOf<Float>(  -0.03f,  0.03f,  0.03f,  -0.03f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
      
-     dir_rr[0][1] = arrayOf<Int>(  0,  1,  2,  3,  10, 10, 10, 10, 10)
+     dir_rr[0][1] = arrayOf<Int>(  0,  1,  2,  3,  10, 10, 10, 10, 10, 10)
      rr[0][1] = arrayOf<Float>(  0.06f,  0.08f,  -0.05f,  -0.05f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
 
      dir_rr[0][2] = arrayOf<Int>(  1,  2,  3,  0,  10, 10, 10, 10, 10, 10)
@@ -112,7 +112,7 @@ class MainActivity : AppCompatActivity() {
      rr[0][3] = arrayOf<Float>(  -0.05f,  -0.06f,  0.03f,  0.02f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
      
      dir_rr[0][4] = arrayOf<Int>(  0,  3,  2,  1,  10, 10, 10, 10, 10, 10)
-     rr[0][4] = arrayOf<Float>(   0.09f,  0.01f,  -0.07f,  -0.02f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
+     rr[0][4] = arrayOf<Float>(   0.09f,  0.01f,  -0.07f,  -0.02f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f) 
 
      dir_rr[0][5] = arrayOf<Int>(  3,  2,  1,  0,  10, 10, 10, 10, 10, 10)
      rr[0][5] = arrayOf<Float>(   0.01f,  -0.07f,  -0.02f,  0.07f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
