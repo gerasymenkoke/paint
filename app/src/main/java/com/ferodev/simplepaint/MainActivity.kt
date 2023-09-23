175707c4f6a233e76f4cb8f096601e055574cf4a
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-09-23 09:42:51 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -1251,11 +1251,13 @@ resmin[jj] [j] = Math.abs ( crn_[j]  - rr[ii] [iii] [j] )
 
                  }                
                          
-          /*   -----------------                                                        
+                                                               
                                      
       if(resnum[l] >= 10 && ((resnum[l-1] <= 9) || (f==1) ) ) { sign = 1; shift1=0 ; ff=1 }
       if( resnum[l] <= 9 && ff==0 )  { iresnum=sresnum[l].toFloat() }              
          
+
+        /*   ----------------
        
                 
       if(sign == 1  && resnum[l-1-shift1]==10 ) {   iresnum= iresnum + xiresnum; xiresnum=0.00f; xsign=1
