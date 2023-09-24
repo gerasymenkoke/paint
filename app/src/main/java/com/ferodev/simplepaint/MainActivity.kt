45254d2d1b66f70f94ea27773d76fa0c00b4c43a
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-09-24 08:48:58 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -85,8 +85,8 @@ class MainActivity : AppCompatActivity() {
     private var asresnum = " "  
  
      
-    private var iresnum = 0.0  // result last operation
-    private var xiresnum = 0.0  // result last operation x or /
+    private var iresnum = 0.0f  // result last operation
+    private var xiresnum = 0.0f  // result last operation x or /
     private var aresnum = " "                 // result each cycle adding as String
     private var aresnum1 = ""  
 
@@ -1255,7 +1255,7 @@ resmin[jj] [j] = Math.abs ( crn_[j]  - rr[ii] [iii] [j] )
                                                                
       if (l==0 &&  resnum[0] == 11)  {  xxsign=-1 }                              
       if(l>0) { if (resnum[l] >= 10 && ((resnum[l-1] <= 9) || (f==1) ) ) { sign = 1; shift1=0 ; ff=1 } }
-      if( resnum[l] <= 9 && ff==0 )  { iresnum=xxsign*sresnum[l].toDouble(); xxsign=1 }              
+      if( resnum[l] <= 9 && ff==0 )  { iresnum=xxsign*sresnum[l].toFloat(); xxsign=1 }              
          
 
    
@@ -1273,28 +1273,27 @@ resmin[jj] [j] = Math.abs ( crn_[j]  - rr[ii] [iii] [j] )
                                                   ffr=0
                                                 }
       
-      if(sign == 1  && resnum[l-1-shift1]==12) { if (shift1==0 && ffr==0) { xiresnum= xsign * sresnum[l-2].toDouble() * sresnum[l].toDouble();ffr=1 
+      if(sign == 1  && resnum[l-1-shift1]==12) { if (shift1==0 && ffr==0) { xiresnum= xsign * sresnum[l-2].toFloat() * sresnum[l].totoFloat();ffr=1 
                                                      iresnum= iresnum - xsign * sresnum[l-2].toFloat()  
                                                                           } 
       
-                                                 else { if (shift1==0 && ffr==1) { xiresnum= xiresnum * sresnum[l].toDouble() } }
-                                                  if (shift1 > 0 && resnum[l] <= 9 ) { xiresnum= xiresnum *  (sresnum[l].toDouble() / sresnum[l-1].toDouble() ) } 
+                                                 else { if (shift1==0 && ffr==1) { xiresnum= xiresnum * sresnum[l].toFloat() } }
+                                                  if (shift1 > 0 && resnum[l] <= 9 ) { xiresnum= xiresnum *  (sresnum[l].toFloat() / sresnum[l-1].toFloat() ) } 
                                                 }
       
-      if(sign == 1  && resnum[l-1-shift1]==13) { if (shift1==0 && ffr==0) { xiresnum= xsign * sresnum[l-2].toDouble() / sresnum[l].toDouble() ; ffr=1 
+      if(sign == 1  && resnum[l-1-shift1]==13) { if (shift1==0 && ffr==0) { xiresnum= xsign * sresnum[l-2].toFloat() / sresnum[l].totoFloat() ; ffr=1 
                                                       iresnum= iresnum - xsign * sresnum[l-2].toFloat()                 
                                                                           } 
-
-          
-                                                 else { if (shift1==0 && ffr==1) { xiresnum= xiresnum / sresnum[l].toDouble() } }
-                                                  if (shift1 > 0 && resnum[l] <= 9 ) { xiresnum= xiresnum /  (sresnum[l].toDouble() * sresnum[l-1].toDouble() ) }  
+         
+                                                 else { if (shift1==0 && ffr==1) { xiresnum= xiresnum / sresnum[l].toFloat() } }
+                                                  if (shift1 > 0 && resnum[l] <= 9 ) { xiresnum= xiresnum /  sresnum[l].toFloat() * sresnum[l-1].toFloat()  }  
                                                   
                                                 } 
      
 
 
      
-      if(l>=1 && resnum[l]==14) { iresnum = iresnum + xiresnum; aresnum =  aresnum + "="  + iresnum.toString()  ; resnum[l+1]=iresnum.toInt(); f=1;  sresnum[l+1]=resnum[l+1]; sign=0; xiresnum=0.00;  ffr=0; xsign=1;  l=l+1; repeat=0 }
+      if(l>=1 && resnum[l]==14) { iresnum = iresnum + xiresnum; aresnum =  aresnum + "="  + iresnum.toString()  ; resnum[l+1]=iresnum.toInt(); f=1;  sresnum[l+1]=resnum[l+1]; sign=0; xiresnum=0.0f;  ffr=0; xsign=1;  l=l+1; repeat=0 }
              
 
      
