a1943a056a7a468e66673404ff715570809a675d
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-10-03 07:51:30 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -373,7 +373,7 @@ class MainActivity : AppCompatActivity() {
 i=0 
 j=0 
 old = 10                    
-while (j >=0 && j<=799) ) {
+while (j >=0 && j<=799)  {
  
                                // dir_crn[i]=dir_cr[j]; // crn_[i]=crx_[j] + cry_[j]; - bad idea + and - annigilate
                                 //  crn_[i]=( ( crn_[i] * 1000.0).roundToInt() / 1000.0).toFloat() 
