ac6cef02f9a3ffbf3fdb02acb7d658efcbce8e22
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-10-09 21:53:41 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -277,8 +277,8 @@ class MainActivity : AppCompatActivity() {
 // +
       jj = 10                
 
-    // xdyrr[10][0]=arrayOf<Float>(   50.0f,  0.0f,  0.0f,  0.0f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
-      xdyrr[10][1]=arrayOf<Float>(   9.0f,  100.0f, 0.0f,  0.0f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
+      xdyrr[10][0]=arrayOf<Float>(   1.0f,  11.0f,  10.0f,  0.11f,  0.00f, -0.07f, -0.18f, -0.16f, -0.20f, 0.0f)
+      xdyrr[10][1]=arrayOf<Float>(   1.0f,  -12.0f, -12.0f, -13.0f,  0.65f, -0.08f, 0.00f, 0.00f, 0.08f, 0.0f)
   
 
 
@@ -289,8 +289,8 @@ class MainActivity : AppCompatActivity() {
 // -
       jj = 11               
   
-        xdyrr[11][0]=arrayOf<Float>(  40.0f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
-        xdyrr[11][1]=arrayOf<Float>(  10.0f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
+        xdyrr[11][0]=arrayOf<Float>(  1.0f,  4.0f,  6.5f, 10.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
+        xdyrr[11][1]=arrayOf<Float>(  1.0f, -6.0f, -3.66f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
 
 
 
