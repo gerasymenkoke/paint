654a0e20c212acc44b0be5e0b86a9ef13e1ebec4
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-10-19 21:56:31 +0300

    Update DrawPencil.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/DrawPencil.kt
@@ -152,7 +152,7 @@ class DrawPencil @JvmOverloads constructor(
         
           
              
-            if ( j<=799 && (j==0 ||  Math.abs(Math.abs(cangle) - Math.abs(cangle1)) > 1 ) ) 
+            if ( j<=799  ) 
         {
          
                  cangle1 = cangle 
@@ -163,11 +163,11 @@ class DrawPencil @JvmOverloads constructor(
                    if (rx==0.0f) rx=0.001f; if (ry==0.001f) ry=0.001f 
                    cangle=  (atan(rx/ry)*(180/PI)).toFloat() 
                  
-                 if (j>=1) {ci=ci+1           
+                 ci=ci+1           
                  cangle_[ci] = cangle 
                  crx_[ci] = rx
                  cry_[ci] = ry
-                           }                                    
+                                                               
                  x1=x
                  y1=y     
                                    
