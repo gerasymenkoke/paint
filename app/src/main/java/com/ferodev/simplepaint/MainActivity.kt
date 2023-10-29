201bb501c116653f2767e00c32a788159a72027b
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-10-29 11:04:50 +0200

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -453,7 +453,7 @@ while ( j <= cin )  {
        j=0;
        cinn=cinn - 1  
 while (j >=0 && j<=cinn) {
-if (j<=cinn) { aaa[j] =  "  [" + j.toString() + "]="  + crnnx_[j].toString() + "," + crnny_[j].toString() + ";;" +canglenn_[j].toString() "/" +canglennpi_[j].toString()  }
+if (j<=cinn) { aaa[j] =  "  [" + j.toString() + "]="  + crnnx_[j].toString() + "," + crnny_[j].toString() + ";;" + canglenn_[j].toString() + "/" + canglennpi_[j].toString()  }
     // 
  // aaa[j] =  "  [" + j.toString() + "]="  +  canglenn_[j].toString() 
 //  aaacr[j] =  "[" + j.toString() + "]=" + "[" + crx_[j].toString() + "," + cry_[j].toString() + "] "  
