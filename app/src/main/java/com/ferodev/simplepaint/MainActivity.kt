f1becd9e2b1a6e95cef0446c23464c9df10abb18
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-11-01 20:52:29 +0200

    Update MainActivity.kt

--- app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
+++ app/src/main/java/com/ferodev/simplepaint/MainActivity.kt
@@ -431,7 +431,7 @@ while ( j <= ci  )  {
         kk=0    
         cinn=0            
 while ( j <= cin )  {
-                      if    (  Math.abs( Math.abs(cangle_[j]) - Math.abs(cangle_[kk]) ) >= 0)
+                      if    (  Math.abs( Math.abs(cangle_[j]) - Math.abs(cangle_[kk]) ) >= 10)
                              
                                          {             
                                          kk=j
