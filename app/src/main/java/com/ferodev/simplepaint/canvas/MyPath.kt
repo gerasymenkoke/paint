1edfa52ef452e091427316f2bd52c9c1045a9431
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-12-17 21:27:28 +0200

    Update MyPath.kt

--- app/src/main/java/com/ferodev/simplepaint/canvas/MyPath.kt
+++ app/src/main/java/com/ferodev/simplepaint/canvas/MyPath.kt
@@ -22,18 +22,6 @@ class MyPath : Path(), Serializable {
         super.reset()
     }
 
-    override fun moveTo(x: Float, y: Float) {
-        actions.add(Move(x, y))
-        super.moveTo(x, y)
-    }
+   
 
-    override fun lineTo(x: Float, y: Float) {
-        actions.add(Line(x, y))
-        super.lineTo(x, y)
-    }
-
-    override fun quadTo(x1: Float, y1: Float, x2: Float, y2: Float) {
-        actions.add(Quad(x1, y1, x2, y2))
-        super.quadTo(x1, y1, x2, y2)
-    }
 }
