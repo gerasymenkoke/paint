c99276b1264aa1d82d5cf2cf3547a3846b28e7d0
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2024-02-24 09:19:26 +0200

    Update MainActivity.kt

--- app/src/main/java/my/app/MainActivity.kt
+++ app/src/main/java/my/app/MainActivity.kt
@@ -16,6 +16,18 @@ import android.graphics.Bitmap
 
 
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+
+
+
+
 
 
 
@@ -1286,19 +1298,85 @@ if (j<=cinn) { aaa[j] =   canglenn_[j].toString() + "/"  }
 
 // keep in storage  canglenn_[j] j=0 to cinn  or aaa[j] as String  if result mismatch
 // write to file
-String filename = "myfile"
-// String[] numbers = new String[] {"1, 2, 3"};
-FileOutputStream outputStream
 
-try {
-  outputStream = openFileOutput(filename, Context.MODE_APPEND);
-  for (String s : aaa) {  
-      outputStream.write(s.getBytes()) 
-  } 
-  outputStream.close()
-} catch (Exception e) {
-  e.printStackTrace()
-}
+
+  override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSION)
+        } else {
+            write()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSION -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                write()
+            }
+        }
+    }
+
+    private fun write() {
+        val dir = "${Environment.getExternalStorageDirectory()}/$packageName"
+        File(dir).mkdirs()
+        val file = "%1\$tY%1\$tm%1\$td%1\$tH%1\$tM%1\$tS.log".format(Date())
+        File("$dir/$file").printWriter().use { 
+            it.println("text")
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
