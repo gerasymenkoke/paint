3f929c91b57ef72a64623241ab9a2031d61c0066
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2024-02-24 09:22:42 +0200

    Update MainActivity.kt

--- app/src/main/java/my/app/MainActivity.kt
+++ app/src/main/java/my/app/MainActivity.kt
@@ -19,10 +19,10 @@ import android.graphics.Bitmap
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+//import android.support.v4.app.ActivityCompat
+//import android.support.v4.content.ContextCompat
 
 
 
