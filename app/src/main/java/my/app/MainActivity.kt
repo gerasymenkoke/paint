8700c6682c1be54b3c6aeff7cf73ca4b49cadfce
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2024-03-03 19:55:35 +0200

    Update MainActivity.kt

--- app/src/main/java/my/app/MainActivity.kt
+++ app/src/main/java/my/app/MainActivity.kt
@@ -35,7 +35,8 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStreamReader
-import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
 
 
 
