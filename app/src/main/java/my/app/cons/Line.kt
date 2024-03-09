aea5c7995ecf8e28c9c27cb04df0c3770aae9680
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2024-03-09 21:54:14 +0300

    Add files via upload

---
+++ app/src/main/java/my/app/cons/Line.kt
@@ -0,0 +1,9 @@
+package com.ferodev.simplepaint.cons
+
+data class Line(
+    var startX: Float = 0f,
+    var startY: Float = 0f,
+    var stopX: Float = 0f,
+    var stopY: Float = 0f,
+    var color: Int = 0
+)
