6684075b6a29c26013ecf30d57ee2b7b87a4f8c3
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2024-03-17 10:34:18 +0200

    Update MainActivity.kt

--- app/src/main/java/my/app/MainActivity.kt
+++ app/src/main/java/my/app/MainActivity.kt
@@ -711,7 +711,12 @@ if ( Math.abs (canglenn_[j] - rcanglenn_[jj] [jjj] [j]) > 90 ) {
             try {
      
                 fileInputStream = FileInputStream( mylastindexFile )  // read from "d+number00" file to data-read as String
-                data_read = fileInputStream.readBytes(4).toString(Charsets.UTF_8)
+               
+                var int size = 4 
+                  data_read  = file.inputStream().use {
+        it.readBytes(size).toString(Charset.forName("UTF-8"))
+                
+          //      data_read = fileInputStream.readBytes(4).toString(Charsets.UTF_8)
                 
             } catch (e: FileNotFoundException){
                 e.printStackTrace()
